c2513fa55a138193b7a7a1af8ff0c98c330a98bd
BrityHemming <hello@brittanyhemming.com>
2020-07-30 08:45:02 -0400

    added challenges and instructions

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # JavaScript-Day-2-Extra-Practice
+
+## Instructions
+
+### Task 1 - Set Up Project
+
+1. Fork repo 
+2. Clone _your_ fork (not original repo by mistake!)
+3. `cd` into your newly cloned repository
+4. Create a new branch by typing `git checkout -b <firstName-lastName>`
+5. Work on your branch, push commits 
+6. When all your tasks are complete DM Brit the link to your repo
+
+### Task 2 - MVP code
+
+Find the file `index.js` and complete the tasks.
---
+++ index.js
@@ -0,0 +1,65 @@
+// Use Array Methods to solve the tasks below 🎉
+
+const toppings = ['Mushrooms ', 'Tomatoes', 'Eggs', 'Chili', 'Lettuce', 'Avocado', 'Chiles', 'Bacon', 'Pickles', 'Onions', 'Cheese'];
+
+/*
+TASK 1 🚀
+// The customer would like to see the topping options, console log out each of the toppings one by one in the array above 
+*/
+
+
+/*
+TASK 2 🚀
+// write a function that accepts an array and an item and removes that item from the array
+for example you no longer have any onions and need to remove it from the list of toppings
+ */
+
+
+
+/*
+TASK 3 🚀
+// Sort the topping alphabetically and return them in a new array 
+*/
+
+
+
+
+const vacations = [
+    {city: 'Toronto', country: 'Canada', region: 'North America', temperature: 86, beach: true, sea: false, wifi:'strong', hiking: false, overall_rating: 6,},
+    {city: 'Miami', country: 'United States', region: 'North America', temperature: 95, beach: true, sea: true, wifi:'strong', hiking: false, overall_rating: 8,},
+    {city: 'Tel Aviv', country: 'Israel', region: 'Middle East', temperature: 102, beach: true, sea: true, wifi:'strong', hiking: true, overall_rating: 10,},
+    {city: 'Istanbul', country: 'Turkey', region: 'Europe', temperature: 86, beach: false, sea: false, wifi:'medium', hiking: true, overall_rating: 7,},
+    {city: 'Bangkok', country: 'Thailand', region: 'South East Asia', temperature: 101, beach: false, sea: false, wifi:'medium', hiking: false, overall_rating: 5,},
+    {city: 'Lima', country: 'Peru', region: 'South America', temperature: 86, beach: false, sea: false, wifi:'weak', hiking: true, overall_rating: 7,},
+    {city: 'Muscat', country: 'Oman', region: 'Middle East', temperature: 105, beach: true, sea: true, wifi:'strong', hiking: true, overall_rating: 9,},
+    {city: 'Sydney', country: 'Australia', region: 'Oceania', temperature: 92, beach: true, sea: true, wifi:'strong', hiking: true, overall_rating: 8,},
+    {city: 'Cape Town', country: 'South Africa', region: 'Africa', temperature: 87, beach: true, sea: true, wifi:'medium', hiking: true, overall_rating: 10,},
+    {city: 'Cancun', country: 'Mexico', region: 'North America', temperature: 95, beach: true, sea: true, wifi:'medium', hiking: false, overall_rating: 7,},
+];
+
+/*
+TASK 4 🚀
+// The travel agent would like to send a couple on their honeymoon to a location with a beach and a temperature above 90 degrees. return their options in a new array 
+*/
+
+
+
+/*
+TASK 5 🚀
+// A developer decides to become a digital nomad for a year, they would like to live in a place with strong wifi, a beach, and good hiking, return their options
+*/
+
+
+
+/* 
+TASK 6 🚀
+// write a function that allows a user to pick 3 preferences from the list above and return a list of places to them matching their preferences 
+*/
+
+
+
+/* 
+TASK 7 🚀
+// Add up all of the overall ratings and find the average rating 
+hint - use .reduce()
+*/
