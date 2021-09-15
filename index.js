6a50b95f04103e3158f37c1d9036c7cb6a8a82b3
Brit Hemming <hello@brittanyhemming.com>
2021-09-15 13:31:54 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,3 @@
-// Use Array Methods to solve the tasks below 🎉
-
-const toppings = ['Mushrooms ', 'Tomatoes', 'Eggs', 'Chili', 'Lettuce', 'Avocado', 'Chiles', 'Bacon', 'Pickles', 'Onions', 'Cheese'];
-
-/*
-TASK 1 🚀
-// The customer would like to see the topping options, console log out each of the toppings one by one in the array above 
-*/
-
-
-/*
-TASK 2 🚀
-// write a function that accepts an array and an item and removes that item from the array
-for example you no longer have any onions and need to remove it from the list of toppings
-Use .forEach() - hint - you will need to include the index in the callback
- */
-
-
-
-/*
-TASK 3 🚀
-// Sort the topping alphabetically and return them in a new array 
-*/
-
-
-
 
 const vacations = [
     {city: 'Toronto', country: 'Canada', region: 'North America', temperature: 86, beach: true, sea: false, wifi:'strong', hiking: false, overall_rating: 6,},
@@ -39,35 +13,35 @@ const vacations = [
 ];
 
 /*
-TASK 4 🚀
+TASK 1 🚀
 // The travel agent would like to send a couple on their honeymoon to a location with a beach and a temperature above 90 degrees. return their options in a new array 
 */
 
 
 
 /*
-TASK 5 🚀
+TASK 2 🚀
 // A developer decides to become a digital nomad for a year, they would like to live in a place with strong wifi, a beach, and good hiking, return their options
 */
 
 
 
 /* 
-TASK 6 🚀
+TASK 3 🚀
 // write a function that allows a user to sort their vacations by hiking opportunities, beach access or a mix of both and return their options
 */
 
 
 
 /* 
-TASK 7 🚀
+TASK 4 🚀
 // write a function that finds the average of overall ratings in a given array. The function should take an array as its argument and should return the average of the overall ratings in that array 
 hint - use .reduce()
 */
 
 
 /*
-TASK 8 🚀
+TASK 5 🚀
 Find the airport codes for each of the cities in the vacation array and write a function to add them to the objects in the array
 hint - your function should include array, index and code as parameters
 you will need to invoke the function each time you wish to add a new code
